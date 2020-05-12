97f1b45d83cb2d5663914c796bc51d1e988526cc
Jeet Karia <50065408+JeetKaria06@users.noreply.github.com>
2020-05-12 15:52:09 +0530

    Nested submissions Visualization with Topics, Verdict and Rating

---
+++ Submission_Visualization/cf-sub-dist3.py
@@ -0,0 +1,59 @@
+import requests
+import json
+import argparse
+import plotly.express as px
+import pandas as pd
+from plotly.subplots import make_subplots
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-handle", "--handle", required=True, help="Handle of the user whose details you are looking for.")
+args = vars(ap.parse_args())
+
+handle = args["handle"]
+response = requests.get("https://codeforces.com/api/user.status?handle="+handle+"&from=1")
+
+if(response.status_code==400):
+    print("No such handle exists :( ")
+    exit()
+
+stat = response.json()['result']
+
+subDict = {}
+
+for submission in stat:
+    # print(submission)
+    # print()
+    for tag in submission['problem']['tags']:
+        if tag not in subDict.keys():
+            subDict[tag] = {}
+        if submission['verdict'] not in subDict[tag].keys():
+            subDict[tag][submission['verdict']] = {}
+        if 'rating' in submission['problem'].keys():
+            subDict[tag][submission['verdict']][str(submission['problem']['rating'])] = []  
+
+# print(subDict)
+
+for submission in stat:
+    for tag in submission['problem']['tags']:
+        if 'rating' in submission['problem'].keys():
+            subDict[tag][submission['verdict']][str(submission['problem']['rating'])].append(submission['problem']['name'])
+
+subData = {}
+dfSub = pd.DataFrame({'tag': [], 'verdict': [], 'rating': [], 'number': []})
+
+for tag in subDict:
+    subData[tag] = subDict[tag]
+    for verdict in subDict[tag]:
+        for rating in subDict[tag][verdict]:
+            subData[tag][verdict][rating] = len(set(subDict[tag][verdict][rating]))
+            dfSub = dfSub.append({'tag': tag, 'verdict': verdict, 'rating': rating, 'number': subData[tag][verdict][rating]}, ignore_index=True)
+
+print(dfSub)
+
+fig = px.sunburst(dfSub, values='number', path=['tag', 'verdict', 'rating'], title='Codeforces Submission Distribution of '+handle)
+# fig.update_layout(uniformtext_minsize=18, uniformtext_mode='hide')
+fig.update_layout(margin = dict(l=0, r=0, b=0))
+# fig.update_layout(hole=.4)
+fig.show()
+
+
